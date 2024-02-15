60161c433389d6356dfe156a10d9e8ec52f4059a
yuta <yuta0525ny@gmail.com>
2024-02-15 09:05:07 +0900

    BLDCのプログラム

--- src/main.cpp
+++ src/main.cpp
@@ -1,288 +1,35 @@
-#include <Arduino.h>
-#include<Adafruit_NeoPixel.h>
-#include<ac.h>
-#include<ball.h>
-#include<line.h>
-#include<motor_a.h>
-#include<MA.h>
-#include<timer.h>
-#include<Cam.h>
+#include<Arduino.h>
+#include <Servo.h>
 
-BALL ball;
-LINE line;
-AC ac;
-motor_attack MOTOR;
+#define MAX_SIGNAL 2000  //PWM信号における最大のパルス幅[マイクロ秒]
+#define MIN_SIGNAL 1000  //PWM信号における最小のパルス幅[マイクロ秒]
+#define ESC_PIN 6  //ESCへの出力ピン
+int volume = 0;  //可変抵抗の値を入れる変数
+char message[50];  //シリアルモニタへ表示する文字列を入れる変数
 
-//======================================================きっく======================================================//
-void kick();
-const int C = 32;
-const int K = 31;
-//======================================================neopiku======================================================//
-#define DELAYVAL 500
-#define PIN        30 
-#define NUMPIXELS 16
-
-int Neo[16] = {12,11,10,9,8,7,6,5,4,3,2,1,0,15,14,13};
-int Neo_p = 999;
-
-Adafruit_NeoPixel pixels(DELAYVAL, PIN, NEO_GRB + NEO_KHZ800);
-//======================================================カメラ======================================================//
-int goal_color = 0;  //青が0 黄色が1
-Cam cam_front(4);
-Cam cam_back(3);
-//======================================================スタートスイッチ======================================================//
-int LED = 13;
-int toogle_f;
-int toogle_P = 27;
-void Switch();
-//======================================================関数たち======================================================/
+Servo esc;  //Servoオブジェクトを作成する．今回はESCにPWM信号を送るので，`esc`と命名している．
 
 void setup() {
   Serial.begin(9600);
-  ball.begin();
-  line.begin();
-  ac.setup();
-  cam_front.begin();
-  cam_back.begin();
-  pixels.begin();
-  pixels.clear();
-  pinMode(LED,OUTPUT);
-  pinMode(K,OUTPUT);
-  pinMode(C,OUTPUT);
-  digitalWrite(C,HIGH);
-  digitalWrite(K,LOW);
-  if(goal_color == 0){
-    cam_front.color = 0;  //青が0 黄色が1
-    cam_back.color = 1;  //青が0 黄色が1
-  }
-  else if(goal_color == 1){
-    cam_front.color = 1;  //青が0 黄色が1
-    cam_back.color = 0;  //青が0 黄色が1
-  }
-  Switch();
+  Serial.println("Program begin...");
+  Serial.println("This program will calibrate the ESC.");
+  Serial.println("Turn on power source, then wait 2 seconds and press any key.");
+  
+  Serial.read();
+
+  esc.attach(ESC_PIN);  //ESCへの出力ピンをアタッチします
+  Serial.println("Writing maximum output.");
+  esc.writeMicroseconds(MAX_SIGNAL);  //ESCへ最大のパルス幅を指示します
+  Serial.println("Wait 2 seconds.");
+  delay(2000);
+  Serial.println("Writing minimum output");
+  esc.writeMicroseconds(MIN_SIGNAL);  //ESCへ最小のパルス幅を指示します
+  Serial.println("Wait 2 seconds. Then motor starts");
+  delay(2000);
 }
 
-void loop() {
-  line.getLINE_Vec();
-  ball.getBallposition();
-  ac.getAC_val();
-  // ball.print();
-  // Serial.print(" | ");
-  // line.print();
-  // Serial.print(" | ");
-  // ac.print();
-  // Serial.print(" | ");
-  // cam_front.print();
-  // Serial.print(" | ");
-  // cam_back.print();
-  // Serial.print(" | ");
-  Serial.println();
-
-  if(ball.ball_get == 1){
-    MOTOR.Moutput(4,200);
-    delay(2000);
-    kick();
-  }
-
-  if(toogle_f != digitalRead(toogle_P)){
-    pixels.clear();
-    MOTOR.motor_0();
-    Switch();
-  }
-}
-
-
-
-void Switch(){
-  digitalWrite(LED,HIGH);
-  toogle_f = digitalRead(toogle_P);
-  delay(100);
-  while(digitalRead(toogle_P) == toogle_f);
-  digitalWrite(LED,LOW);
-  ac.setup_2();
-  toogle_f = digitalRead(toogle_P);
-  delay(100);
-  while(digitalRead(toogle_P) == toogle_f);
-  toogle_f = digitalRead(toogle_P);
-}
-
-
-
-void kick(){
-  // esc.writeMicroseconds(1000);
-  digitalWrite(C,LOW);
-  delay(10);
-  digitalWrite(K,HIGH);
-  digitalWrite(LED,HIGH);
-  delay(10);
-  digitalWrite(K,LOW);
-  digitalWrite(LED,LOW);
-  delay(10);
-  digitalWrite(C,HIGH);
-  // MOTOR.Moutput(4,-200);
-}
-
-
-
-void serialEvent3(){
-  uint8_t reBuf[5];
-  if(Serial3.available() < 5){
-    return;
-  }
-
-  for(int i = 0; i < 5; i++){
-    reBuf[i] = Serial3.read();
-  }
-  while(Serial3.available()){
-    Serial3.read();
-  }
-
-  if(reBuf[0] == 38 && reBuf[4] == 37){
-    if(reBuf[3] == 0){
-      cam_back.on = 0;
-    }
-    else{
-      if(cam_back.color == reBuf[1]){
-        cam_back.on = 1;
-        cam_back.Size = reBuf[3];
-        cam_back.ang = -(reBuf[2] - 127);
-      }
-      else{
-        cam_back.on = 0;
-      }
-    }
-  }
-
-  for(int i = 0; i < 5; i++){
-    // Serial.print(reBuf[i]);
-    // Serial.print(" ");
-  }
-  // Serial.println();
-  // Serial.print("sawa");
-}
-
-
-
-void serialEvent4(){
-  uint8_t reBuf[5];
-  if(Serial4.available() < 5){
-    return;
-  }
-
-  for(int i = 0; i < 5; i++){
-    reBuf[i] = Serial4.read();
-  }
-  while(Serial4.available()){
-    Serial4.read();
-  }
-
-  if(reBuf[0] == 38 && reBuf[4] == 37){
-    if(reBuf[3] == 0){
-      cam_front.on = 0;
-    }
-    else{
-      if(cam_front.color == reBuf[1]){
-        cam_front.on = 1;
-        cam_front.Size = reBuf[3];
-        cam_front.ang = -(reBuf[2] - 127);
-      }
-      else{
-        cam_front.on = 0;
-      }
-    }
-  }
-  for(int i = 0; i < 5; i++){
-    Serial.print(reBuf[i]);
-    Serial.print(" ");
-  }
-  // Serial.println();
-  // Serial.print("sawa");
-}
-
-
-
-void serialEvent6(){
-  // Serial.print(" sawa ");
-  uint8_t read[6];
-  int n = 1;
-  if(Serial6.available() < 6){
-    return;
-  }
-  read[0] = Serial6.read();
-  if(read[0] != 38){
-    return;
-  } 
-  while(0 < Serial6.available()){
-    if(n < 6){
-      read[n] = Serial6.read();
-    }
-    else{
-      Serial6.read();
-    }
-    n++;
-  }
-
-  if(read[0] == 38 && read[5] == 37){
-    line.data_byte[0] = read[1];
-    line.data_byte[1] = read[2];
-    line.data_byte[2] = read[3];
-    line.data_byte[3] = read[4];
-  }
-  else{
-    // Serial.print(" Error!! ");
-  }
-  // for(int i = 0; i < 6; i++){
-  //   Serial.print(read[i]);
-  //   Serial.print(" ");
-  // }
-  // Serial.println();
-}
-
-
-
-void serialEvent8(){
-  int n;
-  int x,y;
-  word revBuf_word[7];
-  byte revBuf_byte[7];
-  //受信データ数が、一定時間同じであれば、受信完了としてデータ読み出しを開始処理を開始する。
-  //受信データあり ※6バイト以上になるまでまつ
-  if(Serial8.available()>= 7){
-    //---------------------------
-    //受信データをバッファに格納
-    //---------------------------
-    n = 0;
-    while(Serial8.available()>0 ){ //受信データがなくなるまで読み続ける
-      //6バイト目まではデータを格納、それ以上は不要なデータであるため捨てる。
-      if(n < 7){
-        revBuf_byte[n] = Serial8.read();   //revBuf_byte[n] = Serial2.read();
-      }
-      else{
-        Serial8.read(); //Serial2.read();  //読みだすのみで格納しない。
-      }
-      n++;
-    }
-    //---------------------------
-    //データの中身を確認
-    //---------------------------
-    //データの先頭、終了コードあることを確認
-    if((revBuf_byte[0] == 0xFF ) && ( revBuf_byte[6] == 0xAA )){
-    //いったんWORD型（16bitデータ）としてから、int16_tとする。
-      revBuf_word[0] = (uint16_t(revBuf_byte[1])<< 8);//上位8ビットをbyteから、Wordに型変換して格納　上位桁にするため8ビットシフト
-      revBuf_word[1] = uint16_t(revBuf_byte[2]);//下位8ビットをbyteから、Wordに型変換して格納      
-      x = int16_t(revBuf_word[0]|revBuf_word[1]);//上位8ビット、下位ビットを合成（ビットのORを取ることで格納する。）
-      // ※int ではなく　int16_t　にすることが必要。intだけだと、32ビットのintと解釈されマイナス値がマイナスとみなされなくなる、int16_tは、16ビット指定の整数型変換
-      revBuf_word[2] = (uint16_t(revBuf_byte[3])<< 8);//上位8ビットをbyteから、Wordに型変換して格納　上位桁にするため8ビットシフト
-      revBuf_word[3] = uint16_t(revBuf_byte[4]);//下位8ビットをbyteから、Wordに型変換して格納      
-      y = int16_t(revBuf_word[2]|revBuf_word[3]);//上位8ビット、下位ビットを合成（ビットのORを取ることで格納する。）
-      // ※int ではなく　int16_t　にすることが必要。intだけだと、32ビットのintと解釈されマイナス値がマイナスとみなされなくなる、int16_tは、16ビット指定の整数型変換
-      ball.get_resister(revBuf_byte[5]);
-
-      x = ball.ball_x.demandAve(x);
-      y = ball.ball_y.demandAve(y);
-    }
-    else{
-      // printf("ERR_REV");
-    }
-  }
+void loop() {  
+  volume = 1100;  //可変抵抗の値を1.0で掛けて変数volumeに入れる．値を調整したい場合は倍率を変更する．
+  Serial.println(volume);  //可変抵抗の値をシリアルモニタに表示
+  esc.writeMicroseconds(volume);  // パルス幅 `volume` のPWM信号を送信する
 }
