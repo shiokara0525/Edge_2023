b6dd88a9c0c4f4707bdced83ae44d40e5fb1cc72
yuta <yuta0525ny@gmail.com>
2024-02-16 16:46:01 +0900

    回ったよ

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 #define MAX_SIGNAL 2000  //PWM信号における最大のパルス幅[マイクロ秒]
 #define MIN_SIGNAL 1000  //PWM信号における最小のパルス幅[マイクロ秒]
-#define ESC_PIN 6  //ESCへの出力ピン
-int volume = 0;  //可変抵抗の値を入れる変数
+#define ESC_PIN 33  //ESCへの出力ピン
+int volume = 0;//可変抵抗の値を入れる変数
 char message[50];  //シリアルモニタへ表示する文字列を入れる変数
 
 Servo esc;  //Servoオブジェクトを作成する．今回はESCにPWM信号を送るので，`esc`と命名している．
@@ -15,8 +15,6 @@ void setup() {
   Serial.println("This program will calibrate the ESC.");
   Serial.println("Turn on power source, then wait 2 seconds and press any key.");
   
-  Serial.read();
-
   esc.attach(ESC_PIN);  //ESCへの出力ピンをアタッチします
   Serial.println("Writing maximum output.");
   esc.writeMicroseconds(MAX_SIGNAL);  //ESCへ最大のパルス幅を指示します
