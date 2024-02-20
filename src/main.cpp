dff1b40362f7477701b38e9e436cfe8b0791239e
yuta <yuta0525ny@gmail.com>
2024-02-20 17:03:03 +0900

    関数にしたりした

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,27 @@
 #define MAX_SIGNAL 2000  //PWM信号における最大のパルス幅[マイクロ秒]
 #define MIN_SIGNAL 1000  //PWM信号における最小のパルス幅[マイクロ秒]
 #define ESC_PIN 33  //ESCへの出力ピン
-int volume = 0;//可変抵抗の値を入れる変数
-char message[50];  //シリアルモニタへ表示する文字列を入れる変数
+#define SIGNAL 1800
 
 Servo esc;  //Servoオブジェクトを作成する．今回はESCにPWM信号を送るので，`esc`と命名している．
+void BLDC_setup();
+void BLDC_run();
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Program begin...");
   Serial.println("This program will calibrate the ESC.");
   Serial.println("Turn on power source, then wait 2 seconds and press any key.");
-  
+  BLDC_setup();
+}
+
+void loop() {
+  BLDC_run();
+}
+
+
+
+void BLDC_setup(){
   esc.attach(ESC_PIN);  //ESCへの出力ピンをアタッチします
   Serial.println("Writing maximum output.");
   esc.writeMicroseconds(MAX_SIGNAL);  //ESCへ最大のパルス幅を指示します
@@ -26,8 +36,7 @@ void setup() {
   delay(2000);
 }
 
-void loop() {  
-  volume = 1500;  //可変抵抗の値を1.0で掛けて変数volumeに入れる．値を調整したい場合は倍率を変更する．
-  Serial.println(volume);  //可変抵抗の値をシリアルモニタに表示
-  esc.writeMicroseconds(volume);  // パルス幅 `volume` のPWM信号を送信する
+
+void BLDC_run(){
+  esc.writeMicroseconds(SIGNAL);  // パルス幅 `volume` のPWM信号を送信する
 }
