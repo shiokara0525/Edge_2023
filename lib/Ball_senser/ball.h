39fe976220ffc25cbc2614006b41455bf7cba76a
yuta <yuta0525ny@gmail.com>
2024-03-13 16:32:14 +0900

    feat(WIP) ボールの通信バイト数を8つにする

--- lib/Ball_senser/ball.h
+++ lib/Ball_senser/ball.h
@@ -8,6 +8,8 @@ class BALL{
         MA ball_x;
         MA ball_y;
         MA far__;
+        MA ball_get_val_1;
+        MA ball_get_val_2;
         float ang;
         float far;
         float far_old = 0;
@@ -16,13 +18,17 @@ class BALL{
         int flag = 1;
         float dx;
         float far_;
+        int get_val;
         int ball_get;
+        int get_th = 80;
         BALL();
         int getBallposition();
         void print();
         void begin();
-        void get_resister(int);
+        void get_resister_1(int);
+        void get_resister_2(int);
 
     private:
-        int get_;
+        int get_1;
+        int get_2;
 };
