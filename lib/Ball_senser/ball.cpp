5b62abdecce8b83091b99540aa98d6694cb1fad4
yuta <yuta0525ny@gmail.com>
2024-03-14 14:33:09 +0900

    update ボールの通信関連を新しくした

--- lib/Ball_senser/ball.cpp
+++ lib/Ball_senser/ball.cpp
@@ -1,8 +1,10 @@
 #include<ball.h>
 
 BALL::BALL(){
-    ball_x.setLenth(10);
-    ball_y.setLenth(10);
+    ball_x.setLenth(1);
+    ball_y.setLenth(1);
+    ball_get_val_1.setLenth(50);
+    ball_get_val_2.setLenth(50);
     far__.setLenth(500);
 }
 
@@ -15,7 +17,9 @@ void BALL::begin(){
 int BALL::getBallposition(){
     float x = ball_x.returnAve();
     float y = ball_y.returnAve();
-    if(x == 0 || y == 0){
+    get_val = get_1 + get_2;
+
+    if(x == 0 && y == 0){
         flag = 0;
         return flag;
     }
@@ -42,7 +46,13 @@ int BALL::getBallposition(){
     if(150 < abs(y)){
         y = (y < 0 ? -150 : 150);
     }
-    ball_get = get_;
+
+    if(50 < get_1 && 50 < get_2 && (52 < get_1 || 52 < get_2) && 105 < get_val){
+        ball_get = 1;
+    }
+    else{
+        ball_get = 0;
+    }
     ang = degrees(atan2(y,x));
     far = sqrt(x_pos*x_pos + y_pos*y_pos);
     dx = (far - far_old);
@@ -54,8 +64,13 @@ int BALL::getBallposition(){
 }
 
 
-void BALL::get_resister(int n){
-    get_ = n;
+void BALL::get_resister_1(int n){
+    get_1 = ball_get_val_1.demandAve(n);
+}
+
+
+void BALL::get_resister_2(int n){
+    get_2 = ball_get_val_2.demandAve(n);
 }
 
 
@@ -70,8 +85,14 @@ void BALL::print(){
     // Serial.print(y_pos);
     // Serial.print(" dx : ");
     // Serial.print(dx);
-    Serial.print(" catch : ");
+    Serial.print(" get_val : ");
+    Serial.print(get_val);
+    Serial.print(" get : ");
     Serial.print(ball_get);
+    Serial.print(" get_1 : ");
+    Serial.print(get_1);
+    Serial.print(" get_2 : ");
+    Serial.print(get_2);
     Serial.print(" flag : ");
     Serial.print(flag);
 }
